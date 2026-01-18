a3ae1a409ca5a48a0b35eacded9a9454c5a4fcf0
Ketbome <Pablo.moraga.san@gmail.com>
2026-01-18 14:03:51 -0300

    chore: adjust Jest coverage thresholds
    - Reduced coverage thresholds for branches from 70 to 60 and functions from 75 to 70 in jest.config.js to better align with project requirements.

--- panel/jest.config.js
+++ panel/jest.config.js
@@ -12,8 +12,8 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 80,
-      branches: 70,
-      functions: 75,
+      branches: 60,
+      functions: 70,
       lines: 80
     }
   },
